144804d48f5fab7d4eedfd4331c9392e18471e61
goembed <zhengsjgoembed@gmail.com>
2016-05-18 20:03:51 +0800

    add b1 b2 b4

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ "$1" == "b4" ];then
 cp board/goembed/b4/335x_b4_defconfig .config
 
+elif [ "$1" == "b2" ];then
+
+cp board/goembed/b2/335x_b2_defconfig .config
+
+elif [ "$1" == "b1" ]; then
+cp board/goembed/b1/335x_b1_defconfig .config
+
+fi
+
 make
+
